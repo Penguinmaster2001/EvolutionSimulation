f3ca93e834a6ad9f440e5ee8e787b22a1ec53eb8
Anthony Cieri <penguinmillion@gmail.com>
2024-02-08 14:11:01 -0800

    feat: Made generation and organism files

---
+++ GenerationalFitness/Generation.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+'''
+    Anthony Cieri
+
+    
+    This module holds the Generation class and functions
+'''
+
+
---
+++ GenerationalFitness/Organism.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+'''
+    Anthony Cieri
+
+    
+    This module holds the Organism class and functions
+'''
+
+class Organism:
+    def __init__(self) -> None:
+        pass
